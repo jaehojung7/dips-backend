bf7218fbcf241b21bf3aa9a68b7e44b2d9cc40be
Seoho Jung <seoho91@gmail.com>
2021-12-20 02:09:27 +0900

    Update common.utils.js

--- common/common.utils.js
+++ common/common.utils.js
@@ -11,7 +11,7 @@ AWS.config.update({
 // (Warning: [DEP0135] DeprecationWarning: ReadStream.prototype.open() is deprecated)
 // Go to package.json, set the version of "resolutions": {"fs-capacitor": "3.0.0"}
 // (Use Apollo Server 2 if this does not work with Apollo Server 3)
-// Make sure the AWS S3 is "ACL-enabled"
+// Make sure the AWS S3 bucket is "ACL-enabled"
 // Run npm install
 export const uploadToS3 = async (file, userId, folderName) => {
   const { filename, createReadStream } = await file;
