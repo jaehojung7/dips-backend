aea2d5f5548069253037e5f7ebab7a1d1670dd79
Seoho Jung <seoho91@gmail.com>
2021-12-25 16:03:39 +0900

    [seoho] make program descriptions editable
    - Later, program content (templates) should be editable as well

---
+++ programs/programs.utils.js
@@ -0,0 +1,7 @@
+export const processHashtags = (description) => {
+  const hashtags = description.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g) || [];
+  return hashtags.map((hashtag) => ({
+    where: { hashtag },
+    create: { hashtag },
+  }));
+};
