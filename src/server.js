3c7b23702980beb61224111395fd3fb25d286d33
Seoho Jung <seoho91@gmail.com>
2022-07-24 22:44:43 +0200

    server build with Babel CLI

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
-import { getUser } from "./users/users.utils";
+import { getUser } from "./src/users/users.utils";
 
 const PORT = process.env.PORT;
 
@@ -12,6 +12,8 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   typeDefs,
   resolvers,
+  playground: true,
+  introspection: true,
 
   // Get user token from the headers and retrieve a logged-in user with the token
   // Add the logged-in user information to context, which is carried by every executed resolver on the server
